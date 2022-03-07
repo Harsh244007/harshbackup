e0d934826a2adbb114ec4f91ef40ab60ded4b6ae
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-03-07 08:28:50 +0530

    m

--- booknameserver/main.js
+++ booknameserver/main.js
@@ -1,26 +1,19 @@
 const express=require('express');
 const app= express();
-
+const data=require('./data')
 
 app.get("/",function (req, res){
     console.log("ha");
     res.send("hello")
 })
 
-var namea={
-harry:
-    {"author1":"Harsh",},
-    herry:{"author2":"hersh",},
-    terry:{"author3":"tersh",},
-    jerry:{"author2":"jersh",},
-    };
 
 app.get("/book",logger, (req, res)=>{
   return res.send("No books")
 })
 
 app.get("/book/:name",auth, (req, res)=>{
-  res.send({bookname:req.name});    
+  res.send({bookname:req.params.name});    
 })
 function logger(req, res, next){
     console.log("Fetching all books")
@@ -28,7 +21,7 @@ function logger(req, res, next){
 }
 function auth(req, res, next){
     console.log(req.params.name);
-    req.name=req.params.name;
+
     next();
 }
 app.listen(4000,() =>{
