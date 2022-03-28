40bcb797e72376c712606e99d6ef3ab125dcc53c
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-03-28 09:28:01 +0530

    m

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// // // console.log("start1")
+
+// // // setTimeout(function (){
+    
+// // //     f(),    console.log("proces")
+
+// // // },0);
+// // // var f=function () {console.log("end")}
+
+// // // function x(){
+// // //     console.log("h")
+// // // }
+// // // function y(x){
+// // //     x()
+// // // }
+// // // y()
+
+// // var obd={
+// //     name:"harsh",
+// //     name2:function (){
+// //     //    console.log(this.name)
+// //     },
+
+// //     add:{
+// //     add2:"h",
+// //     add3:function  (n){ 
+// //         console.log(this.add2+n)
+// //     }}
+// // }
+// // obd.add.add3("a");
+
+// var a=["hane"]
+// a[100]=["harsg"]
+// console.log(a.length)
+
+
+// var bear={sound:"h"}
+// function  roar(){
+// var sound="g",
+//     console.log(this.sound)
+// }
+if(null==undefined)
+console.log("no")
