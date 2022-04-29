a46fa1c75b11a4f8d60a97c37ece11fa38cab855
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-04-30 00:00:31 +0530

    main

--- boilercode.jsx
+++ boilercode.jsx
@@ -1,33 +1,30 @@
-function runProgram(input){
-let a,b,c,d,i,j,k,l;
-let m=1,n=2,count=0;
-    input=input.trim().split("\n");
-    i1=input[0].trim().split(" ").map(Number)
-    i2=input[1].trim().split(" ").map(Number)
-  
-  
-  
-  
-  
-     
-  }
-  if (process.env.USER === "") {
-    runProgram(``);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
+  function runProgram(input){
+  let a,b,c,d,i,j,k,l;
+  let m=1,n=2,count=0;
+      input=input.trim().split("\n");
+      i1=input[0].trim().split(" ").map(Number)
+      i2=input[1].trim().split(" ").map(Number)
+    
+    
+      
+    }
+    if (process.env.USER === "") {
+      runProgram(``);
+    } else {
+      process.stdin.resume();
+      process.stdin.setEncoding("ascii");
+      let read = "";
+      process.stdin.on("data", function (input) {
+        read += input;
+      });
+      process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+      });
+      process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+      });
+    }
